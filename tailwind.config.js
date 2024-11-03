c0e7b92071c7dfa7cecd58f62d7185f66f323c11
seyeong-han <illuminate.han@gmail.com>
2024-11-02 23:50:40 -0500

    feat: add fade animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeInUp: {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        fadeOutDown: {
+          "0%": { opacity: 1, transform: "translateY(0)" },
+          "100%": { opacity: 0, transform: "translateY(-20px)" },
+        },
+      },
+      animation: {
+        fadeInUp: "fadeInUp 0.3s ease-out",
+        fadeOutDown: "fadeOutDown 0.3s ease-in",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
