a3d2054e9599d307c5edba2f6a88c0ccf116a2bc
seyeong-han <h960213@gmail.com>
2024-11-02 19:19:15 -0500

    feat: add /design path

--- src/approuter.jsx
+++ src/approuter.jsx
@@ -5,12 +5,14 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "@aws-amplify/ui-react/styles.css";
 
 import Home from "./pages/Home";
+import Design from "./pages/Design";
 
 const AppRouter = function () {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/design" element={<Design />} />
       </Routes>
     </>
   );
