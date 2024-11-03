90dc0b510942a637dbfb3caed01eb05311995794
seyeong-han <illuminate.han@gmail.com>
2024-11-03 09:38:30 -0600

    init: imagepath

---
+++ src/imagepaths.jsx
@@ -0,0 +1,3 @@
+export { default as bedroomImage } from "./assets/bedroom.jpg";
+export { default as kitchenImage } from "./assets/kitchen.jpg";
+export { default as livingroomImage } from "./assets/livingroom.jpg";
