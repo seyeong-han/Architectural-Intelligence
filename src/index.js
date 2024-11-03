94d28e0d59d8dc0446bb405125fa0a25afbefa2d
seyeong-han <illuminate.han@gmail.com>
2024-11-02 23:45:49 -0500

    fix: rm strictMode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ import amplifyconfig from "./amplifyconfiguration.json";
 Amplify.configure(amplifyconfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
-);
+root.render(<AppRouter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
