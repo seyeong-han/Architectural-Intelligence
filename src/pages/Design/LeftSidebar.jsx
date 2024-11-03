1bfd51313aae89b4a819246f8168566ebeef5d7a
seyeong-han <illuminate.han@gmail.com>
2024-11-03 07:16:11 -0600

    fix: navigate to home

--- src/pages/Design/LeftSidebar.jsx
+++ src/pages/Design/LeftSidebar.jsx
@@ -1,24 +1,23 @@
 // LeftSidebar.js
 import React from "react";
-import {
-  Files,
-  Settings,
-  DollarSign,
-  FolderOpen,
-  ShoppingBag,
-  Home,
-} from "lucide-react";
+import { Home } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function LeftSidebar() {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="w-16 bg-white shadow-md flex flex-col items-center py-4 space-y-6 rounded-3xl ml-4 mt-4 mb-4">
-      {[Home, Files, Settings, DollarSign, FolderOpen, ShoppingBag].map(
-        (Icon, index) => (
-          <button key={index} className="p-2 rounded-lg hover:bg-gray-100">
-            <Icon className="h-6 w-6 text-gray-600" />
-          </button>
-        )
-      )}
+      <button
+        className="p-2 rounded-lg hover:bg-gray-100"
+        onClick={handleHomeClick}
+      >
+        <Home className="h-6 w-6 text-gray-600" />
+      </button>
     </div>
   );
 }
