2d20e7355ad752c9488f5d6e75baf8ecc0a81970
seyeong-han <illuminate.han@gmail.com>
2024-11-03 07:32:45 -0600

    fix: update contents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import { bedroomImage, kitchenImage, livingroomImage } from "../imagepaths";
 
 const commonStyles = {
   button:
@@ -15,107 +18,83 @@ const commonStyles = {
     "relative inline-flex items-center justify-center px-6 py-3 text-base font-bold text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900",
 };
 
+const images = [
+  {
+    original: bedroomImage,
+    thumbnail: "bedroom",
+  },
+  {
+    original: kitchenImage,
+    thumbnail: "kitchen",
+  },
+  {
+    original: livingroomImage,
+    thumbnail: "livingroom",
+  },
+];
+
 const Home = () => {
   const [expanded, setExpanded] = useState(false);
 
   return (
     <div className="relative min-h-screen">
-      <header className="absolute inset-x-0 top-0 z-10 py-4 bg-transparent sm:py-5">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <nav className="flex items-center justify-between">
-            <div className="flex shrink-0">
-              <a href="#" title="Auraui" className="flex">
-                <img
-                  className="w-auto h-8"
-                  src="https://www.auraui.com/logo-light.png"
-                  alt="Auraui Logo"
-                  width={150}
-                  height={40}
-                />
-              </a>
-            </div>
+      <section className="relative min-h-screen bg-white flex items-center justify-center">
+        <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl flex flex-col lg:flex-row items-center justify-between">
+          <div className="w-full lg:w-1/2">
+            <h1 className={commonStyles.sectionTitle}>
+              Architectural Intelligence
+            </h1>
+            <p className={commonStyles.sectionSubtitle}>
+              Renovation meets AI: Transform your home with our AI-powered
+              remodeling tool.
+            </p>
 
-            <div className="flex lg:hidden">
-              <button
-                type="button"
-                className="text-gray-900"
-                onClick={() => setExpanded(!expanded)}
-                aria-expanded={expanded}
-              >
-                {expanded ? (
-                  <FaTimes className="w-7 h-7" />
-                ) : (
-                  <FaBars className="w-7 h-7" />
-                )}
-              </button>
-            </div>
+            <p className="mt-10 text-base font-bold text-gray-900">
+              Key features:
+            </p>
+            <ul className="mt-4 space-y-4">
+              <li className={commonStyles.listItem}>
+                <AiOutlineArrowRight className={commonStyles.listItemIcon} />
+                <span className={commonStyles.listItemText}>
+                  Interactive Design and Remodeling Tools
+                </span>
+              </li>
+              <li className={commonStyles.listItem}>
+                <AiOutlineArrowRight className={commonStyles.listItemIcon} />
+                <span className={commonStyles.listItemText}>
+                  Transform Your Home with AI style generation
+                </span>
+              </li>
+              <li className={commonStyles.listItem}>
+                <AiOutlineArrowRight className={commonStyles.listItemIcon} />
+                <span className={commonStyles.listItemText}>
+                  Replace your old furniture with AI-generated designs
+                </span>
+              </li>
+            </ul>
 
-            <div className="hidden lg:flex">
+            <div className="relative inline-flex mt-10 group">
+              <div className="absolute inset-0 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"></div>
               <a
-                href="#"
-                className={`${commonStyles.button} text-white bg-gray-900 hover:bg-gray-700 focus:ring-gray-900`}
+                href="/design"
+                className={commonStyles.gradientButton}
                 role="button"
               >
-                AI Remodeler
+                Get Started!!
               </a>
             </div>
-          </nav>
-        </div>
-      </header>
+          </div>
 
-      <section className="relative pb-12 bg-white pt-28 sm:pb-16 sm:pt-32 lg:pb-20 xl:pt-48 xl:pb-32">
-        <div className="absolute inset-0">
-          <img
-            className=" object-cover w-full h-full md:block"
-            src="https://images.unsplash.com/photo-1473091534298-04dcbce3278c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Hero Background"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-
-        <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-          <div className="flex">
-            <div className="w-full md:max-w-xs lg:max-w-md">
-              <h1 className={commonStyles.sectionTitle}>AI meets Renovation</h1>
-              <p className={commonStyles.sectionSubtitle}>
-                Transform your home with our AI-powered remodeling tool.
-              </p>
-
-              <p className="mt-10 text-base font-bold text-gray-900">
-                Join our newsletter to:
-              </p>
-              <ul className="mt-4 space-y-4">
-                <li className={commonStyles.listItem}>
-                  <AiOutlineArrowRight className={commonStyles.listItemIcon} />
-                  <span className={commonStyles.listItemText}>
-                    Interactive Design and Remodeling Tools
-                  </span>
-                </li>
-                <li className={commonStyles.listItem}>
-                  <AiOutlineArrowRight className={commonStyles.listItemIcon} />
-                  <span className={commonStyles.listItemText}>
-                    Transform Your Home with AI style generation
-                  </span>
-                </li>
-                <li className={commonStyles.listItem}>
-                  <AiOutlineArrowRight className={commonStyles.listItemIcon} />
-                  <span className={commonStyles.listItemText}>
-                    Replace your old furniture with AI-generated designs
-                  </span>
-                </li>
-              </ul>
-
-              <div className="relative inline-flex mt-10 group">
-                <div className="absolute inset-0 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"></div>
-                <a
-                  href="#"
-                  className={commonStyles.gradientButton}
-                  role="button"
-                >
-                  Get Started!!
-                </a>
-              </div>
+          <div className="w-full lg:w-1/2 mt-10 lg:mt-0">
+            <div className="rounded-xl overflow-hidden">
+              <ImageGallery
+                items={images}
+                showThumbnails={false}
+                showFullscreenButton={false}
+                showPlayButton={false}
+                autoPlay={true}
+                showNav={false}
+              />
             </div>
           </div>
         </div>
